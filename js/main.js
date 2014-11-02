64b27b5e937e3b2029f4d9fcb8305dc96344486b
maitabe <martushb@gmail.com>
2014-11-03 01:23:58 +0200

    layout progress

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+//initializing
+window.onload = function ()
+{
+	//myFunction('quick-reports');
+	document.getElementById('onload-tab').click();
+}
+// select each tab -table mechanism-
+function myFunction(selectedId, tabBtn) {
+
+	// get all tabs elements
+	var contentTabs = document.getElementsByClassName('tab-content');
+
+	// hide all tabs
+ 	for ( var i=0; i<contentTabs.length; i++)
+    {
+      contentTabs[i].style.display ='none';
+    }
+
+	// show selected tab
+	var selectedTab = document.getElementById(selectedId);
+	selectedTab.style.display = 'block';
+
+	// color to default all tabs
+	var defaultTabs = document.getElementsByClassName('plain-tab');
+
+	// hide all tabs
+ 	for ( var i=0; i<defaultTabs.length; i++)
+    {
+      defaultTabs[i].style.background = 'darkgrey';
+    }
+
+	// color selected tab
+	tabBtn.style.background = '#ebebeb';
+}
 
